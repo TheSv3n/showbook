aacc530107c3038dee9f5476d56726500e04a7ed
Steven Creighton <ste.creighton@googlemail.com>
2022-01-29 17:04:59 +0000

    29/01/2022

--- frontend/src/components/Review.js
+++ frontend/src/components/Review.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { Col } from "react-bootstrap";
+import { Form, Button, Row, Col, Container } from "react-bootstrap";
 import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
 import axios from "axios";
@@ -10,6 +10,8 @@ const Review = ({ review, performances, type }) => {
   const [userName, setUserName] = useState("");
   const [performanceDate, setPerformanceDate] = useState("");
   const [performanceVenue, setPerformanceVenue] = useState("");
+  const [showEditFields, setShowEditFields] = useState("");
+  const [editedComment, setEditedComment] = useState("");
 
   const userLogin = useSelector((state) => state.userLogin);
   const { userInfo } = userLogin;
@@ -36,11 +38,17 @@ const Review = ({ review, performances, type }) => {
   };
 
   const editHandler = () => {
-    //TODO
+    setShowEditFields(!showEditFields);
+    setEditedComment(review.comment);
   };
 
   const deleteHandler = () => {
     //TODO
+  };
+
+  const submitHandler = () => {
+    //TODO
+    setShowEditFields(!showEditFields);
   };
 
   useEffect(() => {
@@ -76,32 +84,54 @@ const Review = ({ review, performances, type }) => {
         <></>
       )}
 
-      <div className="text-light">{review.comment} </div>
-      <div className="d-flex justify-content-between">
-        <span className="align-right">
-          {userInfo && userInfo._id === review.user ? (
-            <>
-              <i
-                className="bi bi-pencil-square text-light review-icon mx-1"
-                onClick={editHandler}
-              ></i>
-              <i
-                className="bi bi-trash text-light review-icon mx-1"
-                onClick={deleteHandler}
-              ></i>
-            </>
-          ) : (
-            ""
-          )}
-        </span>
-        {type === "showcomment" ? (
-          ""
-        ) : (
-          <Link to={`/showreview/${review._id}`} className="text-white">
-            <span className="align-left">Show full review</span>
-          </Link>
-        )}
-      </div>
+      {showEditFields ? (
+        <Form onSubmit={submitHandler}>
+          <Form.Group controlId="comment">
+            <Form.Control
+              as="textarea"
+              rows={5}
+              placeholder="Enter comment"
+              value={editedComment}
+              onChange={(e) => setEditedComment(e.target.value)}
+            ></Form.Control>
+          </Form.Group>
+          <Button type="submit" variant="primary" className="my-2">
+            Submit
+          </Button>
+          <Button variant="danger" className="my-2 mx-2" onClick={editHandler}>
+            Cancel
+          </Button>
+        </Form>
+      ) : (
+        <>
+          <div className="text-light">{review.comment} </div>
+          <div className="d-flex justify-content-between">
+            <span className="align-right">
+              {userInfo && userInfo._id === review.user ? (
+                <>
+                  <i
+                    className="bi bi-pencil-square text-light review-icon mx-1"
+                    onClick={editHandler}
+                  ></i>
+                  <i
+                    className="bi bi-trash text-light review-icon mx-1"
+                    onClick={deleteHandler}
+                  ></i>
+                </>
+              ) : (
+                ""
+              )}
+            </span>
+            {type === "showcomment" ? (
+              ""
+            ) : (
+              <Link to={`/showreview/${review._id}`} className="text-white">
+                <span className="align-left">Show full review</span>
+              </Link>
+            )}
+          </div>
+        </>
+      )}
     </li>
   );
 };
